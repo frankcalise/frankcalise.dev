ba205608661ebaf59154ed29b14f3eccd2398a78
Frank Calise <fcalise@gmail.com>
2025-06-03 21:30:43 -0400

    refactor(app): remove tabs, index only

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function IndexPage() {
+  return (
+    <ThemedView>
+      <ThemedText>Frank Calise</ThemedText>
+    </ThemedView>
+  );
+}
